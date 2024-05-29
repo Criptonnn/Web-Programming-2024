14f4a395b705abdbfd5c5dccaab35fa0d21601aa
Criptonnn <eemaemir1@gmail.com>
2024-05-29 10:41:07 +0200

    swagger-php base url hard-coded

--- assets/php/public/v1/docs/swagger.php
+++ assets/php/public/v1/docs/swagger.php
@@ -5,7 +5,9 @@ require __DIR__ . '/../../../vendor/autoload.php';
 if($_SERVER["SERVER_NAME"] == "localhost" || $_SERVER["SERVER_NAME"] == "127.0.0.1") {
     define('BASE_URL', 'http://localhost/Web-Programming-2024/assets/php/');
 } else {
-    define('BASE_URL', $_SERVER["SERVER_NAME"] . "assets/php/"); // SERVER["SERVER_NAME"] = https://king-prawn-app-rtyg8.ondigitalocean.app/
+
+    //define('BASE_URL', $_SERVER["SERVER_NAME"] . "assets/php/"); // SERVER["SERVER_NAME"] = https://goldfish-app-vdz96.ondigitalocean.app/
+    define("BASE_URL", "https://goldfish-app-vdz96.ondigitalocean.app/assets/php/");
 }
 
 error_reporting(0);
